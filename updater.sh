458af637e14a7bd923ea04929c8eabce98cefb0d
Fellipe Castro <contact@fellipecastro.com>
2015-01-31 13:36:59 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,9 +1,9 @@
 for i in {1..2}
 do
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get autoremove
-    sudo apt-get clean
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get autoremove -y
+    sudo apt-get clean -y
     sudo pip install --upgrade setuptools
     sudo pip install --upgrade pip
     sudo pip3 install --upgrade pip
