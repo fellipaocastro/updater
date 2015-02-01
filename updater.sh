5b178ecbf5c72656ef9225cd4aff36cbf2289f57
Fellipe Castro <contact@fellipecastro.com>
2015-02-01 08:24:11 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,11 +1,8 @@
 for i in {1..2}
 do
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get autoremove -y
-    sudo apt-get clean -y
-    sudo pip install --upgrade setuptools
-    sudo pip install --upgrade pip
+    sudo apt-get update upgrade autoremove clean -y
+    sudo pip install --upgrade setuptools pip
     sudo pip3 install --upgrade pip
     sudo pip freeze | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
+    sudo pip3 freeze | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
 done
