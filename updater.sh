75675cc99f5f3b18ccf8953f1454e5c93885d5cf
Fellipe Castro <contact@fellipecastro.com>
2015-12-28 19:40:52 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -5,6 +5,7 @@ sudo apt-get clean -y
 sudo aptitude update -y
 sudo aptitude upgrade -y
 sudo aptitude clean -y
-sudo pip2 install --upgrade pip setuptools
-sudo pip3 install --upgrade pip setuptools
+sudo pip2 install --upgrade pip setuptools virtualenv virtualenvwrapper flake8 ipdb httpie argparse
+sudo pip3 install --upgrade pip setuptools flake8 ipdb httpie argparse --upgrade
 sudo gem update
+sudo gem cleanup
