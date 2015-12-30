77e6505f7b8fc83b304c3edbd16d6243693ff555
Fellipe Castro <contact@fellipecastro.com>
2015-12-30 16:06:25 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,8 +9,8 @@ sudo pip2 install --upgrade pip setuptools virtualenv virtualenvwrapper flake8 i
 sudo pip3 install --upgrade pip setuptools flake8 ipdb httpie argparse --upgrade
 gem update
 gem cleanup
-cd ~/gitconfig && git fetch && git rebase && cd -
 cd ~/ubuntu-updater && git fetch && git rebase && cd -
+cd ~/gitconfig && git fetch && git rebase && cd -
 cd ~/.vim-ide && git fetch && git rebase && cd -
 vim +PluginInstall +qall
 if [[ "$SHELL" == "/bin/zsh" ]]; then
