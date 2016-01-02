f2ee54692a1a745f0ceb313585c1949847cec7cf
Fellipe Castro <contact@fellipecastro.com>
2016-01-02 11:06:42 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,19 +2,26 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get clean -y
+
 sudo aptitude update -y
 sudo aptitude upgrade -y
 sudo aptitude clean -y
+
 sudo pip2 install --upgrade pip setuptools virtualenv virtualenvwrapper flake8 ipdb httpie argparse
 sudo pip3 install --upgrade pip setuptools flake8 ipdb httpie argparse --upgrade
+
 gem update
 gem cleanup
+
 cd ~/ubuntu-updater && git fetch && git rebase && cd -
+
 cd ~/dotfiles && git fetch && git rebase && cd -
 source  ~/.gitconfig
 source  ~/.zsh_aliases
+
 cd ~/vim-ide && git fetch && git rebase && cd -
 vim +PluginInstall +qa
+
 if [[ "$SHELL" == "/bin/zsh" ]]; then
     rehash
 elif [[ "$SHELL" == "/bin/bash" ]]; then
