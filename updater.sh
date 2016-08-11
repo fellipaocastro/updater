0891d8d2ecac3e2cc46d6b384138f66d59934f0a
Fellipe Castro <contact@fellipecastro.com>
2016-08-11 13:29:46 -0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -21,7 +21,7 @@ fi
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo pip2 install -U pip setuptools virtualenv virtualenvwrapper flake8 ipdb httpie argparse
-    sudo pip3 install -U pip setuptools wheels flake8 ipdb argparse
+    sudo pip3 install -U pip setuptools wheel flake8 ipdb argparse
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     pip2 install -U pip setuptools virtualenv virtualenvwrapper flake8 ipdb httpie argparse
     pip3 install -U pip setuptools wheel flake8 ipdb argparse
