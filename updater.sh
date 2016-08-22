8801799017a46d59c97b03d96a13587544c714a1
Fellipe Castro <contact@fellipecastro.com>
2016-08-22 10:22:29 -0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,7 +9,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo aptitude upgrade -y
     sudo aptitude clean -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo softwareupdate --install --all
+    sudo softwareupdate --verbose --install --all
 
     brew update
     brew upgrade
