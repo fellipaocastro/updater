6859f78a25cf309a0ddec55f1d1b97673c1e5dbd
Fellipe Castro <contact@fellipecastro.com>
2017-07-10 18:26:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ rm -rf ~/updater
 git clone https://github.com/fellipecastro/updater.git ~/updater
 
 ln -s ~/updater/updater.sh ~/updater.sh
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo -e '\nexport PATH="/usr/local/sbin:$PATH"' >> ~/.zshrc
+fi
