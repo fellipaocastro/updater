6528b531c756acaeee712e70452215fc0930c037
Fellipe Castro <contact@fellipecastro.com>
2019-02-16 10:04:23 -0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -28,68 +28,75 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 if [ -d "$HOME/updater" ]; then
-    cd $HOME/updater
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/updater
+        && git fetch --all
+        && git rebase
+    )
 fi
 
 if [ -d "$HOME/dotfiles" ]; then
-    cd $HOME/dotfiles
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/dotfiles
+        && git fetch --all
+        && git rebase
+    )
     source  ~/.zsh_aliases
     source  ~/.zsh_functions
     tmux start-server \; source-file ~/.tmux.conf
 fi
 
 if [ -d "$HOME/dotfiles_work" ]; then
-    cd $HOME/dotfiles_work
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/dotfiles_work
+        && git fetch --all
+        && git rebase
+    )
     source  ~/.zsh_work
 fi
 
 if [ -d "$HOME/vim-ide" ]; then
-    cd $HOME/vim-ide
-    git fetch --all
-    git rebase
-    cd -
+    (
+        $HOME/vim-ide
+        && git fetch --all
+        && git rebase
+    )
     vim +PluginInstall +qa
 fi
 
 if [ -d "$HOME/zsh-syntax-highlighting" ]; then
-    cd $HOME/zsh-syntax-highlighting
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/zsh-syntax-highlighting
+        && git fetch --all
+        && git rebase
+    )
     source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
 
 if [ -d "$HOME/dracula_zsh" ]; then
-    cd $HOME/dracula_zsh
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/dracula_zsh
+        && git fetch --all
+        && git rebase
+    )
 fi
 
 if [ -d "$HOME/dracula_iterm" ]; then
-    cd $HOME/dracula_iterm
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/dracula_iterm
+        && git fetch --all
+        && git rebase
+    )
 fi
 
 if [ -d "$HOME/dracula_terminal_app" ]; then
-    cd $HOME/dracula_terminal_app
-    git fetch --all
-    git rebase
-    cd -
+    (
+        cd $HOME/dracula_terminal_app
+        && git fetch --all
+        && git rebase
+    )
 fi
 
-pip2 install --upgrade pip
 pip install --upgrade pip
 
 pyenv activate jupyter3
