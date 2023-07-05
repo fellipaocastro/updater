2d982dcf812fc2945475e084eb0449dba5fffda0
Fellipão Castro <fellipao.castro@gmail.com>
2023-07-05 15:14:02 -0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo '[!] Ubuntu -----------------------------------------------------------------------------------------'
+    echo -e '\n[!] Ubuntu -----------------------------------------------------------------------------------------\n'
 
     sudo apt-get update -y
     sudo apt-get upgrade -y
@@ -11,7 +11,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo aptitude upgrade -y
     sudo aptitude clean -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo '[!] macOS ------------------------------------------------------------------------------------------'
+    echo -e '\n[!] macOS ------------------------------------------------------------------------------------------'
 
     sudo softwareupdate --schedule off
     sudo softwareupdate --verbose --install --all
@@ -31,7 +31,7 @@ fi
 
 if [ -d "$HOME/updater" ]; then
     (
-        echo '[!] updater ----------------------------------------------------------------------------------------'
+        echo -e '\n[!] updater ----------------------------------------------------------------------------------------'
 
         cd $HOME/updater && \
         git fetch --all && \
@@ -41,8 +41,8 @@ fi
 
 if [ -d "$HOME/dotfiles" ]; then
     (
-        echo '[!] dotfiles ---------------------------------------------------------------------------------------'
-    
+        echo -e '\n[!] dotfiles ---------------------------------------------------------------------------------------'
+
         cd $HOME/dotfiles && \
         git fetch --all && \
         git rebase
@@ -54,8 +54,8 @@ fi
 
 if [ -d "$HOME/dotfiles_work" ]; then
     (
-        echo '[!] dotfiles_work ----------------------------------------------------------------------------------'
-    
+        echo -e '\n[!] dotfiles_work ----------------------------------------------------------------------------------'
+
         cd $HOME/dotfiles_work && \
         git fetch --all && \
         git rebase
@@ -65,8 +65,8 @@ fi
 
 if [ -d "$HOME/vim-ide" ]; then
     (
-        echo '[!] vim-ide ----------------------------------------------------------------------------------------'
-    
+        echo -e '\n[!] vim-ide ----------------------------------------------------------------------------------------'
+
         $HOME/vim-ide && \
         git fetch --all && \
         git rebase
@@ -76,8 +76,8 @@ fi
 
 if [ -d "$HOME/zsh-syntax-highlighting" ]; then
     (
-        echo '[!] zsh-syntax-highlighting ------------------------------------------------------------------------'
-    
+        echo -e '\n[!] zsh-syntax-highlighting ------------------------------------------------------------------------'
+
         cd $HOME/zsh-syntax-highlighting && \
         git fetch --all && \
         git rebase
@@ -87,8 +87,8 @@ fi
 
 if [ -d "$HOME/dracula_zsh" ]; then
     (
-        echo '[!] dracula_zsh ------------------------------------------------------------------------------------'
-    
+        echo -e '\n[!] dracula_zsh ------------------------------------------------------------------------------------'
+
         cd $HOME/dracula_zsh && \
         git fetch --all && \
         git rebase
@@ -97,8 +97,8 @@ fi
 
 if [ -d "$HOME/dracula_iterm" ]; then
     (
-        echo '[!] dracula_iterm ----------------------------------------------------------------------------------'
-    
+        echo -e '\n[!] dracula_iterm ----------------------------------------------------------------------------------'
+
         cd $HOME/dracula_iterm && \
         git fetch --all && \
         git rebase
@@ -107,18 +107,18 @@ fi
 
 if [ -d "$HOME/dracula_terminal_app" ]; then
     (
-        echo '[!] dracula_terminal_app ---------------------------------------------------------------------------'
-    
+        echo -e '\n[!] dracula_terminal_app ---------------------------------------------------------------------------'
+
         cd $HOME/dracula_terminal_app && \
         git fetch --all && \
         git rebase
     )
 fi
 
-echo '[!] pip upgrade ------------------------------------------------------------------------------------'
+echo -e '\n[!] pip upgrade ------------------------------------------------------------------------------------'
 pip install --upgrade pip
 
-echo '[!] pyenv update -----------------------------------------------------------------------------------'
+echo -e '\n[!] pyenv update -----------------------------------------------------------------------------------'
 pyenv update
 
 pyenv activate jupyter
@@ -129,21 +129,21 @@ pyenv activate tools
 pip install --upgrade pip
 pyenv deactivate
 
-echo '[!] asdf update ------------------------------------------------------------------------------------'
+echo -e '\n[!] asdf update ------------------------------------------------------------------------------------'
 asdf update
 asdf plugin-update --all
 npm i -g npm
 
-echo '[!] docker cleansing -------------------------------------------------------------------------------'
+echo -e '\n[!] docker cleansing -------------------------------------------------------------------------------'
 docker system prune -af
 docker image prune -af
 docker container prune -f
 docker volume prune -f
 
-echo '[!] omz update -------------------------------------------------------------------------------------'
+echo -e '\n[!] omz update -------------------------------------------------------------------------------------'
 omz update
 
-echo '[!] rehash -----------------------------------------------------------------------------------------'
+echo -e '\n[!] rehash -----------------------------------------------------------------------------------------'
 if [[ "$SHELL" == "/bin/zsh" ]]; then
     rehash
 elif [[ "$SHELL" == "/bin/bash" ]]; then
